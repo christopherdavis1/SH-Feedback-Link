119b6d838fc2b8b56a277fe6541fa9c24507929a
Chris Davis <cdavis565@gmail.com>
2018-04-19 19:46:57 -0400

    Added a web view embedded in a navigation controller Added actions for refresh, back and forward. Currently linked to Trello.com

--- SH Feedback Link/ViewController.swift
+++ SH Feedback Link/ViewController.swift
@@ -13,21 +13,26 @@ class ViewController: UIViewController {
     // Outlets
     @IBOutlet weak var feedbackButtonContiner: UIView!
     
+    
     // Actions
-    @IBAction func feedbackButton(_ sender: Any) {}
+    @IBAction func feedbackButton(_ sender: Any) {
+        performSegue(withIdentifier: "feedbackView", sender: "https://www.trello.com")
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // feedbackButtonContiner.layer.shadowPath = UIBezierPath(rect: feedbackButtonContiner.bounds).cgPath
-        
-        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let identifier = segue.identifier, identifier == "feedbackView" {
+            let toNav = segue.destination as! UINavigationController
+            let toVC = toNav.viewControllers.first as! WebViewController
+            toVC.urlString = sender as! String
+        }
     }
 
 
---
+++ SH Feedback Link/WebViewController.swift
@@ -0,0 +1,64 @@
+//
+//  WebViewController.swift
+//  SH Feedback Link
+//
+//  Created by Christopher Davis on 4/19/18.
+//  Copyright © 2018 Christopher Davis. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var webView: WKWebView!
+    var urlString: String!
+    
+    
+    // MARK: - Actions
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    @IBAction func goBack(_ sender: Any) {
+        webView.goBack()
+    }
+    @IBAction func goForward(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    
+        if keyPath == "estimatedProgress" {
+            if webView.estimatedProgress == 1.0 {
+                navigationItem.title = webView.title
+            } else {
+                navigationItem.title = "Loading..."
+            }
+        }
+    }
+    
+
+    
+// Closing Bracket
+}
